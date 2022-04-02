0de6ee655abb653c9e9f77dabb761230a88cdb15
jordanstotts <jordanstotts22@gmail.com>
2022-04-02 14:05:48 -0500

    insight to next year component created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Home from './components/Home';
 import PlayerBios from './components/PlayerBios';
 import FlagRecipients from './components/FlagRecipients';
 import BootRecipients from './components/BootRecipients';
+import InsightToNextYear from './components/InsightToNextYear';
 import Rules from './components/Rules';
 import History from './components/History'
 
@@ -16,6 +17,7 @@ function App() {
         <Route path='/PlayerBios' element={<PlayerBios />} />
         <Route path='/FlagRecipients' element={<FlagRecipients />} />
         <Route path='/BootRecipients' element={<BootRecipients />} />
+        <Route path='/InsightToNextYear' element={<InsightToNextYear />} />
         <Route path='/History' element={<History />} />
         <Route path='/Rules' element={<Rules />} />
       </Routes>
---
+++ frontend/src/components/InsightToNextYear.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const InsightToNextYear = () => {
+    
+
+    return (
+        <div>
+            Next Year Predictions
+        </div>
+    )
+}
+
+
+export default InsightToNextYear
