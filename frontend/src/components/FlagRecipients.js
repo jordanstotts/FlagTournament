841afcf25b4dce5225e4e4f5f9d6b6381640009d
jordanstotts <jordanstotts22@gmail.com>
2022-04-06 12:33:21 -0500

    flag recipient component styled for desktop

--- frontend/src/components/FlagRecipients.js
+++ frontend/src/components/FlagRecipients.js
@@ -22,7 +22,6 @@ import flag2019 from "../FlagPics/flagWinner2019.jpg";
 import flag2020 from "../FlagPics/flagWinner2020.jpg";
 import flag2021 from "../FlagPics/flagWinner2021.jpg";
 
-
 const flagwinners = [
   {
     id: 1,
@@ -83,9 +82,16 @@ const flagwinners = [
   {
     id: 9,
     name: "Derrick Wiley",
-    course: "Quarry Oaks Golf CLub",
+    course: "Quarry Oaks Golf Club",
     year: 2021,
     flagpic: flag2021,
+  },
+  {
+    id: 10,
+    name: null,
+    course: null,
+    year: null,
+    flagpic: null,
   },
 ];
 
@@ -105,7 +111,7 @@ const FlagRecipients = () => {
       <SiteNavBar />
       <h1 className="flagRecipientHeader">Flag Recipients</h1>
       <div className="flagRecipientLayout">
-        {flagwinners.map((flagwinners) => {
+        {/* {flagwinners.map((flagwinners) => {
           return (
             <Card className="flagRecipientCard">
               <CardBody>
@@ -117,59 +123,213 @@ const FlagRecipients = () => {
                 className="flagRecipientPic"
                 alt="flag recipient"
                 src={flagwinners.flagpic}
-                />
+              />
             </Card>
           );
-        })}
-        </div>
-          <div className="modalGroup">
-
-        <button className="openModalBtn" onClick={() => setOpenModal1(true)}>
-          Scorecard
-        </button>
-        {openModal1 && <Scorecard1 closeModal1={setOpenModal1} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal2(true)}>
-          Scorecard
-        </button>
-        {openModal2 && <Scorecard2 closeModal2={setOpenModal2} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal3(true)}>
-          Scorecard
-        </button>
-        {openModal3 && <Scorecard3 closeModal3={setOpenModal3} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal4(true)}>
-          Scorecard
-        </button>
-        {openModal4 && <Scorecard4 closeModal4={setOpenModal4} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal5(true)}>
-          Scorecard
-        </button>
-        {openModal5 && <Scorecard5 closeModal5={setOpenModal5} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal6(true)}>
-          Scorecard
-        </button>
-        {openModal6 && <Scorecard6 closeModal6={setOpenModal6} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal7(true)}>
-          Scorecard
-        </button>
-        {openModal7 && <Scorecard7 closeModal7={setOpenModal7} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal8(true)}>
-          Scorecard
-        </button>
-        {openModal8 && <Scorecard8 closeModal8={setOpenModal8} />}
-
-        <button className="openModalBtn" onClick={() => setOpenModal9(true)}>
-          Scorecard
-        </button>
-        {openModal9 && <Scorecard9 closeModal9={setOpenModal9} />}
+        })} */}
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Grant Luther</p>
+            <p>Toad Valley</p>
+            <p>2013</p>
           </div>
-      <div></div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2013}
+          />
+         
+          <button className="openModalBtn" onClick={() => setOpenModal1(true)}>
+            Scorecard
+          </button>
+          {openModal1 && <Scorecard1 closeModal1={setOpenModal1} />}
+
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Derrick Wiley</p>
+            <p>The Legacy Golf Club</p>
+            <p>2014</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2014}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal2(true)}>
+            Scorecard
+          </button>
+          {openModal2 && <Scorecard2 closeModal1={setOpenModal2} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Grant Luther</p>
+            <p>Honey Creek Golf Club</p>
+            <p>2015</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2015}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal3(true)}>
+            Scorecard
+          </button>
+          {openModal3 && <Scorecard3 closeModal3={setOpenModal3} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Derrick Wiley</p>
+            <p>Bos Landen Golf Club</p>
+            <p>2016</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2016}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal4(true)}>
+            Scorecard
+          </button>
+          {openModal4 && <Scorecard4 closeModal4={setOpenModal4} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Connor Girkin</p>
+            <p>Blue Top Ridge at Riverside</p>
+            <p>2017</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2017}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal5(true)}>
+            Scorecard
+          </button>
+          {openModal5 && <Scorecard5 closeModal5={setOpenModal5} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Ryan Biegger</p>
+            <p>Tournament Club of Iowa</p>
+            <p>2018</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2018}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal6(true)}>
+            Scorecard
+          </button>
+          {openModal6 && <Scorecard6 closeModal6={setOpenModal6} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Ryan Biegger</p>
+            <p>Amana Colonies Golf Club</p>
+            <p>2019</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2019}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal7(true)}>
+            Scorecard
+          </button>
+          {openModal7 && <Scorecard7 closeModal7={setOpenModal7} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Derrick Wiley</p>
+            <p>Whitebirch at Breezy Point</p>
+            <p>2020</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2020}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal8(true)}>
+            Scorecard
+          </button>
+          {openModal8 && <Scorecard8 closeModal8={setOpenModal8} />}
+        </div>
+
+        <div className="flagRecipientCard">
+          <div className="flagRecipientTitle">
+            <p>Derrick Wiley</p>
+            <p>Quarry Oaks Golf Club</p>
+            <p>2021</p>
+          </div>
+          <img
+            className="flagRecipientPic"
+            alt="flag recipient"
+            src={flag2021}
+          />
+          <button className="openModalBtn" onClick={() => setOpenModal9(true)}>
+            Scorecard
+          </button>
+          {openModal9 && <Scorecard9 closeModal9={setOpenModal9} />}
+        </div>
+
+      </div>
+
+
+      {/* <button className="openModalBtn" onClick={() => setOpenModal1(true)}>
+        Scorecard
+      </button>
+      {openModal1 && <Scorecard1 closeModal1={setOpenModal1} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal2(true)}>
+        Scorecard
+      </button>
+      {openModal2 && <Scorecard2 closeModal2={setOpenModal2} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal3(true)}>
+        Scorecard
+      </button>
+      {openModal3 && <Scorecard3 closeModal3={setOpenModal3} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal4(true)}>
+        Scorecard
+      </button>
+      {openModal4 && <Scorecard4 closeModal4={setOpenModal4} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal5(true)}>
+        Scorecard
+      </button>
+      {openModal5 && <Scorecard5 closeModal5={setOpenModal5} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal6(true)}>
+        Scorecard
+      </button>
+      {openModal6 && <Scorecard6 closeModal6={setOpenModal6} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal7(true)}>
+        Scorecard
+      </button>
+      {openModal7 && <Scorecard7 closeModal7={setOpenModal7} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal8(true)}>
+        Scorecard
+      </button>
+      {openModal8 && <Scorecard8 closeModal8={setOpenModal8} />}
+
+      <button className="openModalBtn" onClick={() => setOpenModal9(true)}>
+        Scorecard
+      </button>
+      {openModal9 && <Scorecard9 closeModal9={setOpenModal9} />}
+      <div></div> */}
     </div>
   );
 };
